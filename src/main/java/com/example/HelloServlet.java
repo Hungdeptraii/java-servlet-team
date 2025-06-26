a6b5cb8a5bb03dce25e345c340dce14bea3edbe8
Trung <hsijjiod@gmail.com>
2025-06-26 14:18:50 +0700

    jv1

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -7,12 +7,23 @@ import jakarta.servlet.annotation.WebServlet;
 
 @WebServlet("/hello9")
 public class HelloServlet extends HttpServlet {
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+        
+        response.setContentType("text/html; charset=UTF-8");
 
-        out.println("<h1>Hello, World, I am a servlet,chú ý nhánh của ---feature hello-19.06.2025!</h1>");
-
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Hello Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hello, World</h1>");
+            out.println("<p>Tôi là một servlet. Chú ý nhánh: <strong>feature/hello-19.06.2025</strong></p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 }
